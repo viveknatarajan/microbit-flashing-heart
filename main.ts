82561c31e15855de09c76452797c53dec9214ea1
VivekNatarajan <natvivek@gmail.com>
2023-12-18 00:30:47 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+
+
+basic.forever(function() {
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.clearScreen()
+    basic.pause(500)
+})
